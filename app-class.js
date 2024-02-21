3044cbc8d14dc0179ecda6b4abfbfc9308c04661
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-21 15:19:09 +0430

    init

--- app-class.js
+++ app-class.js
@@ -8,6 +8,25 @@ function getElement(selection) {
 
 class Counter {
     constructor(element, value) {
+        this.counter = element
+    this.value = value
+    this.resetBtn = element.querySelector('.rest')
+    this.increaseBtn = element.querySelector('.increase')
+    this.decreaseBtn = element.querySelector('.decrease')
+    this.valueDOM = element.querySelector('.value')
+    this.valueDOM.textContent = this.value
+
+    // bind this to all function
+    this.increase = this.increase.bind(this)
+    this.decrease = this.decrease.bind(this)
+    this.reset = this.reset.bind(this)
+
+    this.increaseBtn.addEventListener('click', this.increase) 
+    this.decreaseBtn.addEventListener('click', this.decrease)
+    this.resetBtn.addEventListener('click', this.reset)
+    }
+
+    increase() {
         
     }
 }

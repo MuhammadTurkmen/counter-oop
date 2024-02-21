fe8393e1c4d65436ce022bc46e47d91a00d2f4b9
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-21 14:14:46 +0430

    init

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ function Counter(element, value) {
     this.increaseBtn = element.querySelector('.increase')
     this.decreaseBtn = element.querySelector('.decrease')
     this.valueDOM = element.querySelector('.value')
-    this.valueDOM = this.value
+    this.valueDOM.textContent = this.value
 }
 
-const firstCounter = new Counter(getElement('first-counter'), 100)
-const secondCounter = new Counter(getElement('second-counter'), 200)
+const firstCounter = new Counter(getElement('.first-counter'), 100)
+const secondCounter = new Counter(getElement('.second-counter'), 200)
 
 
 function getElement(selection) {
     const element = document.querySelector(selection)
-    console.log(element)
     if(element) {
         return element
     }
     throw new Error(`please check "${selection}" selector, no such element exists`)
 }
+
 

914b853ec0d56d4842f4723d8def0fc4bfed2c19
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-21 14:06:09 +0430

    init

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function Counter(element, value) {
     this.counter = element
     this.value = value
+    this.resetBtn = element.querySelector
 }
 
 const firstCounter = new Counter(getElement('first-counter'), 100)

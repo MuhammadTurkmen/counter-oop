c3fe56afcc7dc67269bc007492a08d5a263cfd79
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-22 15:24:50 +0430

    init

--- app-class.js
+++ app-class.js
@@ -43,4 +43,4 @@ class Counter {
 
 
 const firstCounter = new Counter(getElement('.first-counter'), 100)
-const secondCounter = new Counter(getElement('.second-counter'), 200) 
+const secondCounter = new Counter(getElement('.second-counter'), 200)  
